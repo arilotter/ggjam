9a0c68fb6a8268cede9fcd37340bd6cfc2cd86cf
Adam Clarke <oreo@throwawaygames.ca>
2015-01-24 18:34:22 -0500

    prefab test

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public Bullet _bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
