05ce53fe6e7347ea8cf3a0689b37f94a076a9578
Ari Lotter <arilotter@gmail.com>
2015-01-24 18:03:56 -0500

    test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	float movespeed = 5;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var direction = new Vector2( Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		direction *= movespeed;
+		rigidbody2D.AddForce(direction);
+	}
+}
