a23f690432393e458448d513abc11e41d706abe8
Ari Lotter <arilotter@gmail.com>
2015-01-24 18:16:12 -0500

    fixt

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,20 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	float movespeed = 5;
 
-	void Start () {
+	float rotationSpeed = 5;
+
+	void Start() {
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update() {
 		var direction = new Vector2( Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
 		direction *= movespeed;
 		rigidbody2D.AddForce(direction);
+		if (direction != Vector2.zero) {
+			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
 	}
 }
