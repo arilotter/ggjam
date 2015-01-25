da2a39b38e326941fb58e613aa04e63d36a0c7dc
Ari Lotter <arilotter@gmail.com>
2015-01-24 20:00:27 -0500

    woo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
-	float movespeed = 10;
+	public float movespeed = 10;
 	float angle;
 
 
-	public string player;
+	public int playerNum;
+	public Weapon weapon;
 
 	void Start() {
 
@@ -16,14 +17,18 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-		var direction = new Vector2(Input.GetAxis("P1 LX"), Input.GetAxis("P1 LY"));
+		var direction = new Vector2(Input.GetAxis("P" + playerNum + " LX"), Input.GetAxis("P" + playerNum + " LY"));
 		direction *= movespeed;
 		rigidbody2D.AddForce(direction);
 
-		var rightJoy = new Vector2(Input.GetAxis ("P1 RX"), Input.GetAxis ("P1 RY"));
+		var rightJoy = new Vector2(Input.GetAxis ("P" + playerNum + " RX"), Input.GetAxis ("P" + playerNum + " RY"));
 		var faceDirection = (rightJoy != Vector2.zero ? rightJoy : (rigidbody2D.velocity));
 
 		angle = Mathf.Atan2(faceDirection.y, faceDirection.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+		if(Input.GetAxis("P" + playerNum + " Shoot") < 0.4) { // right trigger pressed
+			weapon.fireGun();
+		}
 	}
 }
