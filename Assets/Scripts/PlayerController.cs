0f7ca2ced5e76e89b1ff02757607b3560d5e71bd
Ari Lotter <arilotter@gmail.com>
2015-01-24 19:26:19 -0500

    wo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,25 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	float movespeed = 10;
-
 	float angle;
-	
+
+
+	public string player;
+
 	void Start() {
 
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		var direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		var direction = new Vector2(Input.GetAxis("P1 LX"), Input.GetAxis("P1 LY"));
 		direction *= movespeed;
 		rigidbody2D.AddForce(direction);
 
-		var faceDirection = new Vector2(Input.GetAxis ("Shoot X"), Input.GetAxis ("Shoot Y"));
-		if (faceDirection != Vector2.zero) {
-			angle = Mathf.Atan2(faceDirection.y, faceDirection.x) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-		}
+		var rightJoy = new Vector2(Input.GetAxis ("P1 RX"), Input.GetAxis ("P1 RY"));
+		var faceDirection = (rightJoy != Vector2.zero ? rightJoy : (rigidbody2D.velocity));
+
+		angle = Mathf.Atan2(faceDirection.y, faceDirection.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
